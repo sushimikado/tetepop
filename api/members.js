4941815cdd1e98d69a683d654869283efad55712
sushimikado <mallowage@gmail.com>
2026-05-15 21:27:54 +0900

    Update members.js

--- api/members.js
+++ api/members.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const NOTION_TOKEN = process.env.NOTION_TOKEN;
-    const NOTION_MEMBERS_DATABASE_ID = process.env.NOTION_DATABASE_ID;
+    const NOTION_MEMBERS_DATABASE_ID = process.env.NOTION_MEMBERS_DATABASE_ID;
 
     function escapeHtml(str) {
       if (!str) return "";
@@ -93,7 +93,7 @@ function getPlatformIcon(url) {
       `;
     }
     
-    const notionRes = await fetch(`https://api.notion.com/v1/databases/${DATABASE_ID}/query`, {
+    const notionRes = await fetch(`https://api.notion.com/v1/databases/${NOTION_MEMBERS_DATABASE_ID}/query`, {
       method: "POST",
       headers: {
         "Authorization": `Bearer ${NOTION_TOKEN}`,
