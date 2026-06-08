fa49a867a5b6ed7590f8bc3dc7874cfb7d1ad705
sushimikado <mallowage@gmail.com>
2026-06-09 02:19:18 +0900

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -313,3 +313,34 @@ if (list && nextBtn && prevBtn) {
   );
 
 }
+
+// =========================
+// バウンスイン
+// =========================
+const observer =
+  new IntersectionObserver(
+    (entries) => {
+
+      entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+
+          entry.target.classList.add(
+            "show"
+          );
+
+        }
+
+      });
+
+    },
+    {
+      threshold: 0.2
+    }
+  );
+
+document
+  .querySelectorAll(".bounce")
+  .forEach(el => {
+    observer.observe(el);
+  });
